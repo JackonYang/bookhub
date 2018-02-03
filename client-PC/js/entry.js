dadda67568e455e847eb0b31f1962718bd1bd5b1
Jackon Yang <i@jackon.me>
2018-02-03 22:16:06 +0800

    react router v4

--- client-PC/js/entry.js
+++ client-PC/js/entry.js
@@ -1,15 +1,62 @@
 import React from "react";
 import ReactDOM from 'react-dom';
 
-class BookList extends React.Component {
+import { HashRouter as Router, Route, Link } from 'react-router-dom'
+
+
+const routes = [
+    {
+        path: '/',
+        exact: true,
+        sidebar: () => <div>图书搜索</div>,
+        main: () => <h2>A List Of Books</h2>
+    },
+    {
+        path: '/add-books',
+        sidebar: () => <div>添加书籍</div>,
+        main: () => <h2>Adding New Books</h2>
+    },
+    {
+        path: '/settings',
+        sidebar: () => <div>设置</div>,
+        main: () => <h2>a bunch of settings</h2>
+    }
+]
+
+class Sidebar extends React.Component {
     render() {
-      return (
-        <h1>Here Is A List Of Books</h1>
+        return (
+            <ul>
+                {routes.map((route, index) => {
+                    return <li key={index}>
+                        <Link to={route.path}> {route.sidebar()} </Link>
+                    </li>
+                })}
+
+            </ul>
+        );
+    }
+}
+
+
+class Index extends React.Component {
+    render() {
+        return (
+            <Router>
+                <div>
+                    <Sidebar />
+
+                    {routes.map((route, index) => {
+                        return <Route key={index} exact={route.exact} path={route.path} render={() => route.main()} />
+                    })}
+
+                </div>
+            </Router>
         );
     }
 }
 
 ReactDOM.render(
-    <BookList />,
+    <Index />,
     document.getElementById('content')
 );
