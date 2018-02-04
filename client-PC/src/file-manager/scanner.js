78e6c88239dcfd0a42448c3dc24dbc09c658eb3b
Jackon Yang <i@jackon.me>
2018-02-05 00:42:00 +0800

    scan files

---
+++ client-PC/src/file-manager/scanner.js
@@ -0,0 +1,61 @@
+const path = require('path');
+const fs = require('fs');
+const wildcard = require('wildcard');
+
+
+const ignorePaths = [
+  '.*',
+  'node_modules',
+  'Library', // Mac
+  'log',
+  'logs',
+  'video-course',
+];
+
+const targetPtn = [
+  '.pdf',
+];
+
+// let STOP_FLAG = false;
+
+function isIgnoreDir(filePath) {
+  return ignorePaths.some(ptn => wildcard(ptn, filePath));
+}
+
+function scanPath(rootPath) {
+  if (!fs.existsSync(rootPath)) {
+    return 0;
+  }
+
+  const stat = fs.lstatSync(rootPath);
+
+  if (stat.isDirectory()) {
+    let cntAdded = 0;
+    const tarPath = fs.readdirSync(rootPath).filter(ele => !isIgnoreDir(ele));
+
+    // count scaned files and log
+
+    tarPath.forEach(ele => {
+      // ignore hidden if configured
+      cntAdded += scanPath(path.join(rootPath, ele)); // recurse
+    });
+    return cntAdded;
+  }
+
+  // else -> file
+  const ext = path.extname(rootPath);
+  if (!targetPtn.includes(ext)) {
+    return 0;
+  }
+
+  const basename = path.basename(rootPath, ext);
+
+  const metaInfo = {
+    rawname: basename,
+    ext,
+  };
+  console.log(metaInfo);
+  return 1;
+}
+
+scanPath('/Users/');
