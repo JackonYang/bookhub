e673b39ef989a95afc4f69f95fa3d318ec322cd9
Jackon Yang <i@jackon.me>
2018-02-06 01:38:03 +0800

    recieve scan:book:found in addWindow

--- client-PC/src/containers/book-add/index.jsx
+++ client-PC/src/containers/book-add/index.jsx
@@ -3,8 +3,17 @@ import TopFixed from '../../components/top-fixed/index';
 import Table from '../../components/table/index';
 import styles from './book-add.scss';
 
+// https://github.com/electron/electron/issues/9920
+const { ipcRenderer } = window.require('electron');
+
 /* eslint-disable react/prefer-stateless-function */
 class BookAdd extends React.Component {
+  componentDidMount() {
+    ipcRenderer.on('scan:book:found', (e, metaInfo) => {
+      console.log(metaInfo);
+    });
+  }
+
   render() {
     return (
       <div className={styles.wrap}>
