1e7e0207b394245d9f65ee3a399f6b061e0d507a
vivian-xu <vivian.zxx53@gmail.com>
2018-02-12 00:49:13 +0800

    增加设置页面

--- client-PC/renderer/containers/preferences/index.jsx
+++ client-PC/renderer/containers/preferences/index.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
+import styles from './preferences.scss';
+
 /* eslint-disable react/prefer-stateless-function */
 class Preferences extends React.Component {
   render() {
     return (
-      <h2>customize the App</h2>
+      <section className={styles.wrap}>
+        <div className={styles.content}>
+          <h1>
+            Settings
+          </h1>
+          <div>
+            <label htmlFor="library">Library Path</label>
+            <input id="library" />
+          </div>
+        </div>
+      </section>
     );
   }
 }
