bcec3c7e289afb81c2eee4703d0db3e1d84686dc
Jackon Yang <i@jackon.me>
2018-02-11 21:29:02 +0800

    separate background window

--- client-PC/main/index.js
+++ client-PC/main/index.js
@@ -3,6 +3,7 @@ const url = require('url');
 const path = require('path');
 
 import createMainWindow from './main_window'
+import createBackgroundWindow from '../background/background_window'
 
 const {
   app,
@@ -27,17 +28,7 @@ app.on('ready', () => {
     app.quit();
   });
 
-  backgroundWindow = new BrowserWindow({
-    show: false,
-    nodeIntegrationInWorker: true,
-  });
-
-  // Load html into window
-  backgroundWindow.loadURL(url.format({
-    pathname: path.join(__dirname, 'public/background/index.html'),
-    protocol: 'file:',
-    slashes: true,
-  }));
+  backgroundWindow = createBackgroundWindow();
 
   // build menu from template
   const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
